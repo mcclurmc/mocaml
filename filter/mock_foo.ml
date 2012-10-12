ba5a6b6e7332fa78b61e123eab0e9d0e20aa423f
Mike McClurg <mike.mcclurg@citrix.com>
2012-10-13 00:35:09 +0100

    Clean up mock_foo.ml, try some new mocking patterns
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- filter/mock_foo.ml
+++ filter/mock_foo.ml
@@ -1,52 +1,70 @@
-module Foo =
+module type FooSIG =
+sig
+	exception E1
+	type 'a foo
+  val func1 : int -> int -> int
+  val func2 : string -> string -> string
+  val funcN : int -> int -> int
+end
+module Foo : FooSIG =
 struct
-	let func1 a b = a + b
-	let func2 a b = a ^ b
-	let funcN a b = a - b
+	exception E1
+	type 'a foo = 'a list
+  let rec func1 a b = a + b (* make sure we can handle this case *)
+	and func2 a = function b -> a ^ b			(* and this case? *)
+	let funcN = func1
 end
 module MockFoo =
 struct
   module type SIG_Foo = module type of Foo
   module Expect =
   struct
-    type f_name = F_func1 | F_func2 | F_funcN
-	exception Invalid_expect
-	exception Not_implemented
+		exception Invalid_expect
+		exception Not_implemented
+		type cond = Before | After
+    type f_name = F_func1 | F_func2 | F_funcN (* Autogen *)
     let exp = Hashtbl.create 10
+		let (cond : (f_name, cond) Hashtbl.t) = Hashtbl.create 10
+		(* Autogen *)
     let func1 f = Hashtbl.add exp F_func1 (`Ty_func1 f)
     let func2 f = Hashtbl.add exp F_func2 (`Ty_func2 f)
     let funcN f = Hashtbl.add exp F_funcN (`Ty_funcN f)
   end
   module Foo : SIG_Foo =
   struct
+		include Foo
     let func1 a b = try begin
   		let f = Expect.(Hashtbl.find exp F_func1) in
   		match f with
   			| `Ty_func1 f -> f a b
-			| `Ty_func2 _ | `Ty_funcN _
-				-> raise Expect.Invalid_expect
-  	end with _ -> raise Expect.Not_implemented
+				| `Ty_func2 _ | `Ty_funcN _
+					-> raise Expect.Invalid_expect
+		end with _ ->
+			(fun _ _ -> raise Expect.Not_implemented) a b
     and func2 a b = try begin
   		let f = Expect.(Hashtbl.find exp F_func2) in
   		match f with
   			| `Ty_func2 f -> f a b
   			| `Ty_func1 _ | `Ty_funcN _
-				-> raise Expect.Invalid_expect
-  	end with _ -> raise Expect.Not_implemented
-	and funcN : int -> int -> int = fun a b -> try begin
-  		let f = Expect.(Hashtbl.find exp F_func2) in
-  		match f with
-			| `Ty_funcN f -> f a b
-  			| `Ty_func2 _ | `Ty_func1 _ 
-				-> raise Expect.Invalid_expect
-  	end with _ -> raise Expect.Not_implemented
+					-> raise Expect.Invalid_expect
+		end with _ ->
+			(fun _ _ -> raise Expect.Not_implemented) a b
+		and funcN a b = (* try *) begin
+			if not Expect.(Hashtbl.mem exp F_funcN)
+			then (fun _ _ -> raise Expect.Not_implemented) a b else
+				let f = Expect.(Hashtbl.find exp F_funcN) in
+				match f with
+					| `Ty_funcN f -> f a b
+					| `Ty_func2 _ | `Ty_func1 _
+						-> raise Expect.Invalid_expect
+		end (* with _ -> *)
+	(* (fun _ _ -> raise Expect.Not_implemented) a b *)
   end
-(*   let expect = *)
-(*   object *)
-(*   	  val exp = Hashtbl.create 10 *)
-(*       method func1 f = Hashtbl.add exp `F_func1 (`Ty_func1 f) *)
-(*   end *)
-(* module Foo2 : SIG_Foo = *)
-(* struct *)
-(* end *)
 end
+
+let _ =
+	MockFoo.Expect.funcN (fun _ _ -> 42) ;
+	MockFoo.Expect.funcN (fun a _ -> if a = 1 then raise Not_found else 42) ;
+	MockFoo.Expect.func2 (fun _ _ -> "42") ;
+	Printf.printf "func2 \"a\" \"b\" = \"%s\"\n" (MockFoo.Foo.func2 "a" "b") ;
+	Printf.printf "funcN 1 2 = %d\n" (MockFoo.Foo.funcN 1 2) ;
