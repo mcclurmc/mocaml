26d79eac389d3ea99d0beb3279bfe850da81ef31
Mike McClurg <mike.mcclurg@citrix.com>
2012-10-16 20:13:28 +0100

    Basic functionality is implemented
    Wow, looks like we might be close to something useful. This generates a
    valid-looking module from an interface. I'll have to check that it compiles,
    and then test out how useful the mock module is.
    
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- filter/mock_filter.ml
+++ filter/mock_filter.ml
@@ -9,120 +9,142 @@ struct
 
 	type func = {
 		name: string;
-		args: Ast.ctyp;
+		ty: Ast.ctyp;
+		f_name: string;
+		f_type: string;
 	}
 
 	let gen =
 		let module P = Camlp4.PreCast.Printers.OCaml in
-		let _loc = Ast.Loc.ghost in
-		let cast_str_item ast =
-			(Obj.magic ast : Camlp4.PreCast.Ast.str_item) in
+		let rec _loc = Ast.Loc.ghost
+		and cast_str_item ast =
+			(Obj.magic ast : Camlp4.PreCast.Ast.str_item)
+		(* and count_args = *)
+		(* 	let rec loop n = function *)
+		(* 		| <:ctyp< $t$ -> $ts$ >> -> loop (n+1) ts *)
+		(* 		| <:ctyp< $t$ >> -> n *)
+		(* 	in loop 0 *)
+		(* and gen_arg_list s n = *)
+		(* 	let rec loop i acc = match n with *)
+		(* 		| i when i > n -> acc *)
+		(* 		| i -> loop (i+1) ((s ^ (string_of_int i)) :: acc) *)
+		(* 	in loop 1 [] *)
+		in
+
 	object(self)
+
 		val expect_excs = (<:str_item<
 			exception Invalid_expect ;
 			exception Not_implemented >>)
 
+		(* XXX: how will we get this name? *)
+		val mutable mname = "Foo"
+
 		val mutable funcs = []
 
+		val hash_name = "e"
+
 		method add_func fn ty =
-			funcs <- {name = fn; args = ty} :: funcs
+			let f_name = "F_" ^ fn in
+			let f_type = "T_" ^ fn in
+			funcs <- {name = fn;
+								ty = ty;
+								f_name = f_name;
+								f_type = f_type} :: funcs
 
+		(* XXX: merge f_name and f_type methods *)
 		method f_name =
-			let mk_cons fn = <:ctyp< $lid: "F_" ^ fn$ >> in
+			let mk_cons f = <:ctyp< $uid:f.f_name$ >> in
 			let rec loop = function
-				| [] -> <:ctyp< >>
-				| f::fs -> <:ctyp< $mk_cons f.name$ | $loop fs$ >>
+				| []    -> <:ctyp< >>
+				| f::fs -> <:ctyp< $mk_cons f$ | $loop fs$ >>
 			in if funcs == []
-			then <:str_item< >>
-			else <:str_item< type f_name = [ $loop (List.rev funcs)$ ] >>
+				then <:str_item< >>
+				else <:str_item< type f_name = [ $loop funcs$ ] >>
 
-		method mk_sig mn =
+		method f_type =
+			let mk_cons f = <:ctyp< $uid:f.f_type$ of $f.ty$ >> in
+			let rec loop = function
+				| []    -> <:ctyp< >>
+				| f::fs -> <:ctyp< $mk_cons f$ | $loop fs$ >>
+			in if funcs = []
+				then <:str_item< >>
+				else <:str_item< type f_type = [ $loop funcs$ ] >>
+
+		method mock_sig mn =
 			<:str_item< module type $"SIG_" ^ mn$ = module type of $uid:mn$ >>
 
 		method module_expect = <:str_item<
 			module Expect = struct
 				$expect_excs$ ;
 				$self#f_name$ ;
+				$self#f_type$ ;
+				value $lid:hash_name$ = Hashtbl.create 10 ;
 			end
 		>>
 
-		method debug =
-			print_endline "\n(* In debug method *)" ;
-			P.print_implem @@ cast_str_item (self#mk_sig "Foo") ;
-			P.print_implem @@ cast_str_item self#module_expect ;
-			print_endline "(* Out of debug method *)\n" ;
+		method gen_mock_funcs =
+			let add_mock = <:str_item<
+				value add_mock fn ty = Hashtbl.add $lid:hash_name$ fn ty >> in
+			let fs = List.fold_left
+				(fun st f ->
+					<:str_item<
+						$st$ ;
+						value $lid:f.name$ = fun
+							[ f -> add_mock $uid:"F_"^f.name$ ( $uid:"T_"^f.name$ f ) ] >>)
+				<:str_item< >>
+				funcs
+			in
+			<:str_item< $add_mock$ ; $fs$ >>
+
+		method module_mock = <:str_item<
+			module $mname$ = struct
+				include $uid:mname$ ;
+				$self#gen_mock_funcs$ ;
+			end
+		>>
+
+		method module_outer =
+			funcs <- List.rev funcs ;
+			<:str_item<
+			module $"Mock" ^ mname$ = struct
+				$self#mock_sig "Foo"$ ;
+				$self#module_expect$ ;
+				$self#module_mock$ ;
+			end
+		>>
+
+		method print =
+			let pp a = P.print_implem @@ cast_str_item a in
+			pp self#module_outer ;
+
 	end
 
-	(* Things to generate:
-	 * - module type SIG_<Module> = module type of <Module>
-	 * - module Expect -- might want this to be an object
-	 *   - exceptions, hashtable (can include this from a prototype
-	 *     module, or a from a superclass
-	 *   - type f_name = F_<func1> | F_<func2> | ...
-	 *   - type f_type = T_<func1> of <ty_arg1> * <ty_arg2> ... | T_<func2> of ...
-	 *   - type conditions = Before | After | ...
-	 *   - let <func1> f = Hashtbl.add e F_<func1> (T_<func1> f, conditions list)
-	 * - module <Module>
-	 *   - include <Module>
-	 *   - let <fun_name> <args list> = <some mock function impl>
-	 *)
-
-	module P = Camlp4.PreCast.Printers.OCaml
-
-	(* So how do I make a filter that turns a sig_item into a str_item?
-		 Let's see how p4_idl.ml does it... Okay, it doesn't do that. What
-		 we'll do is make a filter that does a noop on the original mli
-		 file, and as a side effect emits a new ml file based on what it
-		 finds in the mli. *)
 	let rec sig_filter = function
 		| <:sig_item< value $lid:bid$ : $typ:ts$ >> as st ->
-				let n = count_args ts in
-				Printf.printf "filtering sig %s, %d args...\n" bid n ;
+				(* let n = count_args ts in *)
+				(* Printf.printf "filtering sig %s, %d args...\n" bid n ; *)
 				let _loc = Ast.loc_of_sig_item st in
-				let st = <:str_item< value $lid:bid$ x = () >> in
-				P.print_implem @@ cast_str_item st ;
 				gen#add_func bid ts ;
-				empty_str_item _loc (* st *)
+				<:str_item< >>
 		| st ->
-				print_endline "ignoring sig..." ;
 				let _loc = Ast.loc_of_sig_item st in
 				<:str_item< >>
 
-	and count_args =
-		let rec loop n = function
-		| <:ctyp< $t$ -> $ts$ >> -> loop (n+1) ts
-		| <:ctyp< $t$ >> -> n
-		in loop 0
-
-	and cast_str_item ast =
-		(Obj.magic ast : Camlp4.PreCast.Ast.str_item)
-
-	and empty_str_item _loc = <:str_item< >>
+	(* and count_args = *)
+	(* 	let rec loop n = function *)
+	(* 	| <:ctyp< $t$ -> $ts$ >> -> loop (n+1) ts *)
+	(* 	| <:ctyp< $t$ >> -> n *)
+	(* 	in loop 0 *)
 	;;
 
   register_sig_item_filter @@ fun si ->
     let _loc = Ast.loc_of_sig_item si in
 		let sis = Ast.list_of_sig_item si [] in
 		let _ = List.map sig_filter sis in
-		gen#debug ;
-    <:sig_item< $list: sis$ >>
+		gen#print ;
+		<:sig_item< >>
   ;;
-
-	(* let rec str_filter = function *)
-	(* 	| <:str_item< value $lid:bid$ $args$ = $stmts$ >> as st -> *)
-	(* 			Printf.printf "filtering str %s...\n" bid ; st *)
-	(* 	| <:str_item< module type $lid:name$ = sig $sg$ end >> -> *)
-	(* 			Printf.printf "filtering mod type %s...\n" name ; *)
-	(* 			sig_filter sg *)
-	(* 	| st -> print_endline "ignoring st..."; st *)
-	(* ;; *)
-
-	(* register_str_item_filter @@ fun st -> *)
-	(* 	let _loc = Ast.loc_of_str_item st in *)
-	(* 	let sts = Ast.list_of_str_item st [] in *)
-	(* 	let sts = List.map str_filter sts in *)
-	(* 	<:str_item< $list: sts$ >> *)
-	(* ;; *)
 
 end
 
