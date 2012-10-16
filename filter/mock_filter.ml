e4fbcbf31bcba081ee65e9d9005c67af9cc034f6
Mike McClurg <mike.mcclurg@citrix.com>
2012-10-16 12:13:46 +0100

    Add object to mock_filter.ml which builds up mock module state for printing
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- filter/mock_filter.ml
+++ filter/mock_filter.ml
@@ -9,8 +9,49 @@ struct
 
 	type func = {
 		name: string;
-	  args: Ast.ctyp list;
+		args: Ast.ctyp;
 	}
+
+	let gen =
+		let module P = Camlp4.PreCast.Printers.OCaml in
+		let _loc = Ast.Loc.ghost in
+		let cast_str_item ast =
+			(Obj.magic ast : Camlp4.PreCast.Ast.str_item) in
+	object(self)
+		val expect_excs = (<:str_item<
+			exception Invalid_expect ;
+			exception Not_implemented >>)
+
+		val mutable funcs = []
+
+		method add_func fn ty =
+			funcs <- {name = fn; args = ty} :: funcs
+
+		method f_name =
+			let mk_cons fn = <:ctyp< $lid: "F_" ^ fn$ >> in
+			let rec loop = function
+				| [] -> <:ctyp< >>
+				| f::fs -> <:ctyp< $mk_cons f.name$ | $loop fs$ >>
+			in if funcs == []
+			then <:str_item< >>
+			else <:str_item< type f_name = [ $loop (List.rev funcs)$ ] >>
+
+		method mk_sig mn =
+			<:str_item< module type $"SIG_" ^ mn$ = module type of $uid:mn$ >>
+
+		method module_expect = <:str_item<
+			module Expect = struct
+				$expect_excs$ ;
+				$self#f_name$ ;
+			end
+		>>
+
+		method debug =
+			print_endline "\n(* In debug method *)" ;
+			P.print_implem @@ cast_str_item (self#mk_sig "Foo") ;
+			P.print_implem @@ cast_str_item self#module_expect ;
+			print_endline "(* Out of debug method *)\n" ;
+	end
 
 	(* Things to generate:
 	 * - module type SIG_<Module> = module type of <Module>
@@ -26,14 +67,6 @@ struct
 	 *   - let <fun_name> <args list> = <some mock function impl>
 	 *)
 
-	(* module PP = Camlp4.Printers.OCaml.Make(Camlp4.PreCast.Syntax) *)
-	(* let pp = new PP.printer ~comments: false () *)
-	(* let print_expr = (new PP.printer ())#expr *)
-
-	(* let print_str_item si = () *)
-
-	(* let (str_pp, sig_pp) = Camlp4.Register.current_printer () *)
-
 	module P = Camlp4.PreCast.Printers.OCaml
 
 	(* So how do I make a filter that turns a sig_item into a str_item?
@@ -48,6 +81,7 @@ struct
 				let _loc = Ast.loc_of_sig_item st in
 				let st = <:str_item< value $lid:bid$ x = () >> in
 				P.print_implem @@ cast_str_item st ;
+				gen#add_func bid ts ;
 				empty_str_item _loc (* st *)
 		| st ->
 				print_endline "ignoring sig..." ;
@@ -60,39 +94,35 @@ struct
 		| <:ctyp< $t$ >> -> n
 		in loop 0
 
-	and bar nm _loc =
-		let _loc = AstFilters.Ast.Loc.ghost in
-		let st = <:str_item< value foo = "Mike was here" >> in
-		cast_str_item st
-
 	and cast_str_item ast =
 		(Obj.magic ast : Camlp4.PreCast.Ast.str_item)
 
 	and empty_str_item _loc = <:str_item< >>
 	;;
 
-	let rec str_filter = function
-		| <:str_item< value $lid:bid$ $args$ = $stmts$ >> as st ->
-				Printf.printf "filtering str %s...\n" bid ; st
-		| <:str_item< module type $lid:name$ = sig $sg$ end >> ->
-				Printf.printf "filtering mod type %s...\n" name ;
-				sig_filter sg
-		| st -> print_endline "ignoring st..."; st
-	;;
-
   register_sig_item_filter @@ fun si ->
     let _loc = Ast.loc_of_sig_item si in
 		let sis = Ast.list_of_sig_item si [] in
 		let _ = List.map sig_filter sis in
+		gen#debug ;
     <:sig_item< $list: sis$ >>
   ;;
 
-	register_str_item_filter @@ fun st ->
-		let _loc = Ast.loc_of_str_item st in
-		let sts = Ast.list_of_str_item st [] in
-		let sts = List.map str_filter sts in
-		<:str_item< $list: sts$ >>
-	;;
+	(* let rec str_filter = function *)
+	(* 	| <:str_item< value $lid:bid$ $args$ = $stmts$ >> as st -> *)
+	(* 			Printf.printf "filtering str %s...\n" bid ; st *)
+	(* 	| <:str_item< module type $lid:name$ = sig $sg$ end >> -> *)
+	(* 			Printf.printf "filtering mod type %s...\n" name ; *)
+	(* 			sig_filter sg *)
+	(* 	| st -> print_endline "ignoring st..."; st *)
+	(* ;; *)
+
+	(* register_str_item_filter @@ fun st -> *)
+	(* 	let _loc = Ast.loc_of_str_item st in *)
+	(* 	let sts = Ast.list_of_str_item st [] in *)
+	(* 	let sts = List.map str_filter sts in *)
+	(* 	<:str_item< $list: sts$ >> *)
+	(* ;; *)
 
 end
 
